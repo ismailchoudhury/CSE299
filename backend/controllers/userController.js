a9ebabd7d9adb39c8fdff9be7b5928d68d1eae8b
Ismail Mobin Choudhury <choudhuryismailmobin@gmail.com>
2023-10-08 06:45:19 +0600

    AdminSchema

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,40 +1,44 @@
-const User = require('../models/userModel')
-const jwt = require('jsonwebtoken')
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
-const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
-}
+const createToken = _id => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
-// login a user
+// Login a user
 const loginUser = async (req, res) => {
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
   try {
-    const user = await User.login(email, password)
+    const user = await User.login(email, password);
 
-    // create a token
-    const token = createToken(user._id)
+    if (!user) {
+      return res.status(400).json({ error: "Invalid Credentials" });
+    }
 
-    res.status(200).json({email, token})
+    // Create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-}
+};
 
-// signup a user
+// Signup a user (with optional super admin flag)
 const signupUser = async (req, res) => {
-  const {email, password} = req.body
+  const { email, password, isSuperAdmin } = req.body;
 
   try {
-    const user = await User.signup(email, password)
+    const user = await User.signup(email, password, isSuperAdmin);
 
-    // create a token
-    const token = createToken(user._id)
+    // Create a token
+    const token = createToken(user._id);
 
-    res.status(200).json({email, token})
+    res.status(200).json({ email, token });
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-}
+};
 
-module.exports = { signupUser, loginUser }
+module.exports = { signupUser, loginUser };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,14 @@
-const express = require('express')
+const express = require("express");
 
-// controller functions
-const { loginUser, signupUser } = require('../controllers/userController')
+// Controller functions
+const { loginUser, signupUser } = require("../controllers/userController");
 
-const router = express.Router()
+const router = express.Router();
 
-// login route
-router.post('/login', loginUser)
+// Login route
+router.post("/login", loginUser);
 
-// signup route
-router.post('/signup', signupUser)
+// Signup route for regular users
+router.post("/signup", signupUser);
 
-module.exports = router
+module.exports = router;
